a768d3c192665626aafd1213131c4a394f263ea1
Alessandro Duterte <alexduterte@gmail.com>
2020-02-03 02:03:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,28 @@
 # Add your code here
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song or number:"
+  input = gets.strip
+  songs.each_with_index do |song, index|
+    num = index + 1
+    if song.include?(input) || num.to_s == input
+      puts "Playing #{num}. #{song}"
+    end
+  end
+end
+
+def list(songs)
+  songs.each_with_index do | song, index |
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def exit_jukebox
+end
